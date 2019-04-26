ef6bab225084644a1706528d7432309643f6de6b
KARTHEEK <KARTHEEKVADDE@users.noreply.github.com>
2019-04-26 15:20:53 +0530

    Create types.go

---
+++ types.go
@@ -0,0 +1,23 @@
+// all numeric types default to 0
+
+// unsigned int with 8 bits
+// Can store: 0 to 255
+var myint uint8
+// signed int with 8 bits
+// Can store: -127 to 127
+var myint int8
+
+// unsigned int with 16 bits
+var myint uint16
+// signed int with 16 bits
+var myint int16
+
+// unsigned int with 32 bits
+var myint uint32
+// signed int with 32 bits
+var myint int32
+
+// unsigned int with 64 bits
+var myint uint64
+// signed int with 64 bits
+var myint int64
