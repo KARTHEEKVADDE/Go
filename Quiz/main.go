1b4588cd60ef2e9c09ac94524a3170da560bc194
KARTHEEK <KARTHEEKVADDE@users.noreply.github.com>
2019-04-28 17:02:01 +0530

    Update main.go

--- Quiz/main.go
+++ Quiz/main.go
@@ -6,23 +6,34 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func main() {
-
-  file,_ := os.Open("file.csv")
-
+	file,_ := os.Open("file.csv")
 	lines,_ := csv.NewReader(file).ReadAll()
 
 	correct := 0
 	for i, line := range lines {
 		fmt.Printf("Problem #%d: %s = ", i+1, line[0])
-		var answer string
-		fmt.Scanf("%s\n", &answer)
-		if answer == strings.TrimSpace(line[1]) {
-			correct++
+		answerCh := make(chan string)
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+    timer := time.NewTimer(5 * time.Second)
+		select {
+		case <-timer.C:
+			fmt.Println()
+			continue
+		case answer := <-answerCh:
+			if answer == strings.TrimSpace(line[1]) {
+				correct++
+			}
+      continue
 		}
-	}
-
+	} 
 	fmt.Printf("You scored %d out of %d.\n", correct, len(lines))
 }
+
